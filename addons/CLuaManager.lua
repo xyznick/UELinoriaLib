47f14520b15508a45c458962696421f7c10bc3f2
nick <safreltolsamel@gmail.com>
2026-09-09 13:49:10 -0300

    add banner

---
+++ addons/CLuaManager.lua
@@ -0,0 +1,16 @@
+--[[
+
+    ____          _                     _                
+  / ___|   _ ___| |_ ___  _ __ ___    | |   _   _  __ _ 
+ | |  | | | / __| __/ _ \| '_ ` _ \   | |  | | | |/ _` |
+ | |__| |_| \__ \ || (_) | | | | | |  | |__| |_| | (_| |
+  \____\__,_|___/\__\___/|_| |_| |_|  |_____\__,_|\__,_| b0.1
+                                                        
+    By xyznick
+
+    CAUTION!!
+
+    Custom Lua is still in BETA, meaning some features might not work
+    Report bugs to my discord: @xyzznick 
+
+]]
